90575f57c1a3292d620d2296c4b6d796fe87c699
HungNV <hungnv@vitalify.asia>
2018-10-16 21:45:19 +0700

    #add bank card

--- flutter_payment/lib/widgets/bank_card.dart
+++ flutter_payment/lib/widgets/bank_card.dart
@@ -61,6 +61,44 @@ class BankCard extends StatelessWidget {
                       fontWeight: FontWeight.bold),
                   ),
                 ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 16.0, top: 4.0),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        'VALID\nTHRU',
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                          color: card.bgAsset == 'images/bg_purple_card.png' || card.bgAsset == 'images/bg_blue_card.png' ? Colors.grey : Colors.black,
+                          fontSize: 10.0,
+                          fontWeight: FontWeight.bold),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                        child: Text(
+                          card.validDate,
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 16.0, top: 4.0),
+                  child: Text(
+                    card.name,
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      color: card.bgAsset == 'images/bg_purple_card.png' || card.bgAsset == 'images/bg_blue_card.png' ? Colors.grey : Color(0xFF253C70),
+                      fontSize: 12.0,
+                      fontWeight: FontWeight.bold),
+                  ),
+                ),
               ],
             ),
           ),
