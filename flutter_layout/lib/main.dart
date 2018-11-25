c6efd18ff4121c1e05d9277a1cde9b8921eaec6b
HungNV <hungnv@vitalify.asia>
2018-11-25 15:26:25 +0700

    add project flutter_layout and flutter_listview

--- flutter_layout/lib/main.dart
+++ flutter_layout/lib/main.dart
@@ -5,23 +5,47 @@ void main() => runApp(new MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        primarySwatch: Colors.blue,
+    Widget titleSection = new Container(
+      padding: const EdgeInsets.all(10.0),
+      child: new Row(
+        children: <Widget>[
+          new Expanded(
+            child: new Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                new Container(
+                  padding: const EdgeInsets.only(bottom: 10.0),
+                  child: new Text("Programming tutorials channel",  
+                    style: new TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18.0
+                      )),
+                ),
+                new Text("This channel contains tutorial videos in Flutter, React Native, React, Angular", 
+                  style: new TextStyle(
+                    color: Colors.grey[600],
+                    fontSize: 16.0))
+              ],
+            ),
+          ),
+          new Icon(Icons.favorite, color: Colors.red),
+          new Text(" 100", style: new TextStyle(fontSize: 16.0)),
+        ],
       ),
+    );
+    return new MaterialApp(
+      title: '',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Top Lakes'),
+          title: Text('Flutter App'),
         ),
         body: ListView(
-          children: [
-            Image.asset(
-              '',
-              width: 600.0,
-              height: 240.0,
+          children: <Widget>[
+            new Image.asset(
+              "images/image.jpg",
               fit: BoxFit.cover,
-              ),
+            ),
+            titleSection,
           ],
         ),
       ),
--- flutter_listview/lib/main.dart
+++ flutter_listview/lib/main.dart
@@ -11,12 +11,69 @@ class RandomEnglishWords extends StatefulWidget {
 }
 
 class RandomEnglishWordsState extends State<RandomEnglishWords> {
+  final _words = <WordPair>[];
+  final _checkedWords = new Set<WordPair>();
   @override
   Widget build(BuildContext context) {
-    final wordPair = new WordPair.random();
-    return new Text(
-      wordPair.asPascalCase,
-      style: TextStyle(fontSize: 22.0),
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("List of English words"),
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.list), 
+          onPressed: _pushToSaveWordsScreen)
+        ],
+      ),
+      body: new ListView.builder(itemBuilder: (context, index) {
+        if (index >= _words.length) {
+          _words.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_words[index], index);
+      }),
+    );
+  }
+
+  _pushToSaveWordsScreen() {
+    // print("You pressed to the right Icon");
+    final pageRoute = new MaterialPageRoute(builder: (context) {
+      final listTitle = _checkedWords.map((wordpair) {
+        return new ListTile(
+          title: new Text(
+            wordpair.asUpperCase,
+            style: new TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+          ),
+        );
+      });
+      return new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Checked words"),
+        ),
+        body: new ListView(children: listTitle.toList(),),
+      );
+    });
+    Navigator.of(context).push(pageRoute);
+  }
+
+  Widget _buildRow(WordPair wordPair, int index) {
+    final textColor = index % 2 == 0 ? Colors.red : Colors.blue;
+    final isChecked = _checkedWords.contains(wordPair);
+    return new ListTile(
+      leading: new Icon(
+        isChecked ? Icons.check_box : Icons.check_box_outline_blank,
+        color: textColor,
+      ),
+      title: new Text(
+        wordPair.asPascalCase,
+        style: new TextStyle(fontSize: 18.0, color: textColor),
+      ),
+      onTap: () {
+        setState(() {
+          if (isChecked) {
+            _checkedWords.remove(wordPair);
+          } else {
+            _checkedWords.add(wordPair);
+          }
+        });
+      },
     );
   }
 }
@@ -26,14 +83,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: "This is my first Flutter App",
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text("This is Header's title"),
-        ),
-        body: new Center(
-          child: new RandomEnglishWords(),
-        ),
-      ),
+      home: new RandomEnglishWords(),
     );
   }
 }
